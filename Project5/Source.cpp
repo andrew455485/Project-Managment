bc268d2934b9095423d40e9a913d1ac8b25d3462
andrew455485 <samirandrew14@gmail.com>
2021-11-01 00:38:48 +0200

    added

---
+++ Project5/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "enter the tow number : " << endl;
+	cin >> x >> y;
+	cout << "sum = " << x + y << endl;
+	system("pause");
+	return 0;
+}
